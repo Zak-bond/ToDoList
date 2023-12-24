e50bc37b8df2a2834d03e49157a5c21d5bcfc1e4
Zak-bond <111093537+Zak-bond@users.noreply.github.com>
2023-12-24 11:33:58 -0500

    To Do List

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -1,40 +1,49 @@
 import { useState } from "react";
 import "./styles.css";
+import { newToDoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
 
 export default function App() {
-  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: newItem,
+          completed: false,
+        },
+      ];
+    });
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id == id) {
+          return {
+            ...todo,
+            completed,
+          };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
 
   return (
     <>
-      <form className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input
-            value={newItem}
-            onChange={(e) => setNewItem(e.target.value)}
-            type="text"
-            id="item"
-          />
-        </div>
-        <button className="btn">Add</button>
-      </form>
+      <newToDoForm onSubmit={addTodo} />
       <h1 className="header">Todo list</h1>
-      <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            item 1
-          </label>
-          <button className="btn btn-danger">delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            item 2
-          </label>
-          <button className="btn btn-danger">delete</button>
-        </li>
-      </ul>
+      <TodoList todos={todos} />
     </>
   );
 }
